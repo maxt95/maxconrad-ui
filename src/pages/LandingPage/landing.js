9b57b8b9278608042c745b7bd925033ca327f8eb
Max Conrad <maxconrad@me.com>
2021-01-19 22:37:50 -0500

    Layout of link blocks sketched out via css

--- src/pages/LandingPage/landing.js
+++ src/pages/LandingPage/landing.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import './landing.scss'
+import avatar from '../../images/avatar.jpg'
 
 const Landing = () => {
   return (
     <div className="landing-page">
       <div className="about-section">
         <div className="avatar">
-          {/* insert photo here */}
+          <img alt="avatar" src={avatar} />
         </div>
-        <div className="description" />
+        <div className="description">
+          <h1>Max Conrad</h1>
+          <h2>Software Engineer</h2>
+        </div>
       </div>
       <div className="link-section">
-
+        <div className="left-padding" />
+        <div className="links">
+          <div className="link"></div>
+          <div className="link"></div>
+          <div className="link"></div>
+          <div className="link"></div>
+        </div>
       </div>
     </div>
   )
