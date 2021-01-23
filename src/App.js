25f50ea1831addbc31388b6cace8ab4e42e3c8d5
Max Conrad <maxconrad@me.com>
2021-01-22 22:43:46 -0500

    Remove unused header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.scss'
-import Header from './components/header'
 import Landing from './pages/LandingPage'
 
 function App() {
   return (
     <div className="App">
-      {/* <Header className="App-header" /> */}
       <Landing />
     </div>
   )
